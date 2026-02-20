5b38f9fceda60bc8fec3fd5733d8756351233225
aru-shi2 <arushimishra2929@gmail.com>
2026-02-20 17:10:00 +0530

    ui tweaks

--- Frontend/src/Components/Info.jsx
+++ Frontend/src/Components/Info.jsx
@@ -30,26 +30,26 @@ const Info = () => {
 
         <div className="about px-6 md:px-10 py-10 grid md:grid-cols-2 gap-10">
 
-         <div className="image bg-linear-to-br from-yellow-200 to-green-200 rounded-lg flex justify-center items-center py-12 transition duration-300 hover:from-yellow-200 hover:to-green-300"> <img src={Data.sprites.other["official-artwork"].front_default} alt="" className="w-60 object-contain transition duration-300 hover:scale-105" /> </div>
+         <div className="image bg-linear-to-br to-yellow-200 from-amber-100 rounded-lg flex justify-center items-center py-12 transition duration-300 hover:from-yellow-200 hover:to-amber-100"> <img src={Data.sprites.other["official-artwork"].front_default} alt="" className="w-60 object-contain transition duration-300 hover:scale-105" /> </div>
 
 
           <div>
-            <h2 className=" text-xl md:text-2xl font-semibold mb-6 text-gray-800">
+            <h2 className=" text-xl md:text-2xl font-semibold px-2 pt-2 pb-2 mb-6 text-gray-800">
               About
             </h2>
 
-            <div className="first flex justify-between py-3 border-b border-gray-300 hover:bg-yellow-50 transition px-2 rounded text-base md:text-lg">
-              <p className="text-gray-600">Species</p>
+            <div className="first flex justify-between py-3 border-b border-gray-300 hover:bg-yellow-50 transition px-2 rounded text-base md:text-xl">
+              <p className="text-gray-700">Species</p>
               <p className="capitalize font-medium text-gray-800">{Data.species.name}</p>
             </div>
 
             <div className="second flex justify-between py-3 border-b border-gray-300 hover:bg-yellow-50 transition px-2 rounded text-base md:text-lg">
-              <p className="text-gray-600">Height</p>
+              <p className="text-gray-700">Height</p>
               <p className="font-medium text-gray-800">{Data.height}</p>
             </div>
 
             <div className="third flex justify-between py-3 border-b border-gray-300 hover:bg-yellow-50 transition px-2 rounded text-base md:text-lg">
-              <p className="text-gray-600">Weight</p>
+              <p className="text-gray-700">Weight</p>
               <p className="font-medium text-gray-800">{Data.weight}</p>
             </div>
 
@@ -59,7 +59,7 @@ const Info = () => {
               </p>
               <ul className="flex flex-wrap gap-3">
                 {Data.abilities.map((i,index)=>(
-                  <li key={index} className="bg-yellow-100 text-yellow-700 px-3 py-1 rounded-md text-sm md:text-base font-medium capitalize hover:bg-yellow-200 hover:-translate-y-0.5 transition duration-200 cursor-pointer">
+                  <li key={index} className="bg-yellow-100 text-yellow-700 px-3 py-1 rounded-md text-sm md:text-lg font-medium capitalize hover:bg-yellow-200 hover:-translate-y-0.5 transition duration-200 cursor-pointer">
                     {i.ability.name}
                   </li>
                 ))}
@@ -91,7 +91,7 @@ const Info = () => {
           </h2>
 
           <ul className="moves grid sm:grid-cols-2 md:grid-cols-3 gap-3">
-            {Data?.moves?.slice(0,10).map((m,index)=>(
+            {Data?.moves?.slice(0,9).map((m,index)=>(
               <li 
                 key={index} 
                 className="bg-yellow-100 text-yellow-800 px-3 py-2 rounded-md text-sm md:text-base text-center capitalize font-medium hover:bg-yellow-200 hover:-translate-y-0.5 transition duration-200 cursor-pointer"
